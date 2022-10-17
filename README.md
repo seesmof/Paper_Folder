6bb019fcd193bc8b1c614f7f68b6f605a93ae3ea
seesmof <110289150+seesmof@users.noreply.github.com>
2022-10-17 18:53:30 +0300

    refactor: :recycle: minor changes
    change links in readme to correct ones; add intro and outro to the program; add comments describing program's workflow

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 <div align="center">
 
 [![Status](https://img.shields.io/badge/status-active-success.svg)]()
-[![GitHub Issues](https://img.shields.io/github/issues/seesmof/The-Documentation-Compendium.svg)](https://github.com/seesmof/BMI_Calculator/issues)
-[![GitHub Pull Requests](https://img.shields.io/github/issues-pr/seesmof/The-Documentation-Compendium.svg)](https://github.com/seesmof/BMI_Calculator/pulls)
+[![GitHub Issues](https://img.shields.io/github/issues/seesmof/The-Documentation-Compendium.svg)](https://github.com/seesmof/Paper_Folder/issues)
+[![GitHub Pull Requests](https://img.shields.io/github/issues-pr/seesmof/The-Documentation-Compendium.svg)](https://github.com/seesmof/Paper_Folder/pulls)
 [![License](https://img.shields.io/badge/license-MIT-blue.svg)](./LICENSE)
 
 </div>
--- index.cpp
+++ index.cpp
@@ -1,13 +1,17 @@
+// include necessary libraries
 #include <iostream>
 #include <string>
 #include <math.h>
 using namespace std;
 
+// declare global variables
 string unit;
 double height, times, i;
 
+// create measurement unit conversion function
 void CONVERT()
 {
+    // for mm unit conversion
     if (unit == "mm")
     {
         if (height >= 10)
@@ -20,6 +24,7 @@ void CONVERT()
             unit = "mm";
         }
     }
+    // for cm unit conversion
     if (unit == "cm")
     {
         if (height >= 100)
@@ -32,6 +37,7 @@ void CONVERT()
             unit = "cm";
         }
     }
+    // for m unit conversion
     if (unit == "m")
     {
         if (height >= 1000)
@@ -46,8 +52,15 @@ void CONVERT()
     }
 }
 
+// start main function
 int main(int argc, char **argv)
 {
+    // output program intro
+    cout << endl;
+    cout << "************************************** Paper Folder *****************************************" << endl
+         << endl;
+
+    // ask user to input paper thickness, unit of measurement and times folded
     cout << "What thickness is your paper?: ";
     cin >> height;
     cout << "Please enter unit of measurement: ";
@@ -56,6 +69,7 @@ int main(int argc, char **argv)
     cin >> times;
     cout << endl;
 
+    // create a simple times converter
     string time;
     if (times == 1)
     {
@@ -66,13 +80,23 @@ int main(int argc, char **argv)
         time = "times";
     }
 
+    // create a for loop for calculating the paper thickness
     for (i = 1; i <= times; i++)
     {
         height = height * 2;
         CONVERT();
         cout << "If folded " << i << " " << time << " the thickness of the paper is " << height << " " << unit << endl;
+        // each time convert unit and output the result of iteration
     }
 
+    // output the results
     cout << endl;
     cout << "Your sheet of paper folded " << i << " " << time << " will be " << height << " " << unit << endl;
+
+    // output program outro
+    cout << endl;
+    cout << "*********************************************************************************************" << endl;
+
+    // end main function
+    system("pause");
 }
